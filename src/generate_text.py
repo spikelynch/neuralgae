5c2216b63c859b8b3cfe4c8c73f986cc26f67765
Mike Lynch <mike.lynch@iinet.net.au>
2015-12-14 21:45:34 +1100

    Added tweet generator

--- src/generate_text.py
+++ src/generate_text.py
@@ -5,6 +5,8 @@ import nltk, random, re, argparse, sys, os.path
 
 TEXT = 'definitions.txt'
 LINELENGTH = 6
+TWEET_CHARS = 116
+
 
 def makePairs(arr):
     pairs = []
@@ -26,17 +28,40 @@ def generate(cfd, word = 'the', num = 50):
     return words
 
 
+def make_tweet(cfd, words, num):
+    outwords = []
+    for w in words:
+        start = w.replace(' ', '_')
+        try:
+            outwords += generate(cfd, start, args.number)
+        except IndexError as e:
+            print "ERROR: %s %s" % ( start, e )
+    outwords = [ w.replace('_', ' ') for w in outwords ]
+    tweet = ""
+    for w in outwords:
+        if len(tweet) + len(w) + 1 > TWEET_CHARS:
+            break
+        tweet += " " + w
+    return tweet
+
+    
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--number", type=int, default=LINELENGTH,help="Words per line")
+parser.add_argument("-t", "--tweets", action='store_true', help="Limit output to %s characters" % TWEET_CHARS)
 parser.add_argument("logfile",  type=str, help="Log file from neuralgia_control")
+
 
 args = parser.parse_args()
 
 if not os.path.isfile(args.logfile):
     print "Could not read %s" % args.logfile
     sys.exit(-1)
+
+
+
+    
 
 
 with open(TEXT, 'r') as f:
@@ -54,15 +79,19 @@ with open(TEXT, 'r') as f:
                 image = m.group(1)
                 bits = m.group(2)
                 words = bits.split(', ')
-                print image
-                for w in words:
-                    start = w.replace(' ', '_')
-                    try:
-                        line = ' '.join(generate(cfd, start, args.number))
-                    except IndexError as e:
-                        print "ERROR: %s %s" % ( start, e )
-                    print line.replace('_', ' ')
-                print "\n"
+                if args.tweets:
+                    tweet = make_tweet(cfd, words, args.number)
+                    print "%s:%s" % ( image, tweet )
+                else:
+                    print image
+                    for w in words:
+                        start = w.replace(' ', '_')
+                        try:
+                            line = ' '.join(generate(cfd, start, args.number))
+                        except IndexError as e:
+                            print "ERROR: %s %s" % ( start, e )
+                        print line.replace('_', ' ')
+                    print "\n"
 
 
 
